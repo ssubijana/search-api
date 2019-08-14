b3337606b06e1a8b5412284d6132f80e1c788395
ssubijana <sara.subijana@gmail.com>
2019-08-14 08:08:53 +0200

    chore(querydsl) : adds maven dependencies

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,1 +1,4 @@
-insert into USER(ID, NAME, FIRST_NAME, LAST_NAME, IDENTITY_DOCUMENT, AGE) values (1, 'USER_1', 'FIRSTNAME', 'LASTNAME', '00000000T', 33)
+insert into USER(ID, NAME, FIRST_NAME, LAST_NAME, IDENTITY_DOCUMENT, AGE) values (1, 'Sara', 'Martinez', 'Vazquez', '00000000T', 33)
+insert into USER(ID, NAME, FIRST_NAME, LAST_NAME, IDENTITY_DOCUMENT, AGE) values (2, 'Alberto', 'Lopez', 'Gimeno', '11111111R', 34)
+insert into USER(ID, NAME, FIRST_NAME, LAST_NAME, IDENTITY_DOCUMENT, AGE) values (3, 'Bego', 'Cuello', 'Baron', '17763843T', 32)
+insert into USER(ID, NAME, FIRST_NAME, LAST_NAME, IDENTITY_DOCUMENT, AGE) values (4, 'Amara', 'Gracia', 'Ruiz', '17763842E', 37)
